4a233669a1dfca501bb7f5b327db8bf09209abb9
pullock <pullock@yandex.ru>
2024-01-10 22:36:32 +0300

    study commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '.buttons',
+    data: {
+        button_1: 'О нас',
+        button_2: 'Товары и услуги',
+        button_3: 'Цены'
+    }
+})
+
+new Vue({
+    el: '.buttons_bottom',
+    data: {
+        button_1: 'Сделать заказ',
+        button_2: 'Отзывы'
+    }
+})
