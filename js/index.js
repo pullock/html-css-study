bbe9ad3d0d2ec9b1de780a78d749e8c4d340be18
pullock <pullock@yandex.ru>
2024-01-22 20:07:34 +0300

    study

--- js/index.js
+++ js/index.js
@@ -14,3 +14,22 @@ new Vue({
         button_2: 'Отзывы'
     }
 })
+
+let app = new Vue({
+    el: '.text',
+    data: {
+        text_one: 'Тут блок ДИВ',
+        text_two: 'Второй текст',
+        change: ''
+    },
+    methods: {
+        changeFunc: function() {
+            this.change == this.text_one ? this.change = this.text_two : this.change = this.text_one;
+        },
+        start: function () {
+            this.change = this.text_one;
+        }
+    }
+})
+
+app.start();
